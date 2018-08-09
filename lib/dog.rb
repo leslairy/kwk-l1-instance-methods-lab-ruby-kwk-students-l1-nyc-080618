07372d81cb299b866d9a9e561dfa49ffaba934ea
Leyla Alekperova <leslairy@gmail.com>
2018-08-09 20:09:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+end
+def bark 
+  puts "Woof!"
+end
+def sit
+puts "The Dog is sitting"
+end
+end
+
+
+
+
+
+
+
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # person.rb
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+  puts "Tne Person is walking"
+end
+end
+  
+  
+  
+  
